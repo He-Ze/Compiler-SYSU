eace6b548f14c0fa33fcf512d4a6402561d7f8b2
HeZe <heze_heze@icloud.com>
2021-04-19 10:11:18 +0800

    修复了无法检测最后一位的错误的bug

--- 实验/实验3/Postfix/src/Postfix.java
+++ 实验/实验3/Postfix/src/Postfix.java
@@ -42,17 +42,19 @@ class Parser {
 
     void rest() throws IOException {
         while (length >= 0) {
-            if (indexOfLookahead < length && Character.isDigit((char) lookahead)) {
+            if (indexOfLookahead <= length && Character.isDigit((char) lookahead)) {
                 print2Locations(indexOfLookahead);
                 System.out.println("这两个运算量间缺少运算符，已自动忽略第二个运算量");
                 System.out.println("------------------------------------------");
-                indexOfLookahead++;
+                if(indexOfLookahead<length)
+                    indexOfLookahead++;
                 lookahead = input.get(indexOfLookahead);
-            } else if (indexOfLookahead < length && lookahead != '+' && lookahead != '-') {
+            } else if (indexOfLookahead <= length && lookahead != '+' && lookahead != '-') {
                 printLocation(indexOfLookahead);
                 System.out.println("非法运算符，已自动忽略，只支持+与-");
                 System.out.println("------------------------------------------");
-                indexOfLookahead++;
+                if(indexOfLookahead<length)
+                    indexOfLookahead++;
                 lookahead = input.get(indexOfLookahead);
             } else if (lookahead == '+') {
                 match('+');
